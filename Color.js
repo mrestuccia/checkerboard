24151b3729b3566e177d121f82bc374749ae9a90
Mauro Restuccia <mrestuccia@mac.com>
2017-12-01 21:59:51 -0500

    fix: redo using jquery

--- Color.js
+++
@@ -1,6 +0,0 @@
-function Color(r, g, b, a) {
-	this.r = r;
-	this.g = g;
-	this.b = b;
-	this.a = (typeof a == "undefined") ? 1 : a;
-}
--- Point.js
+++
@@ -1,5 +0,0 @@
-function Point(x, y, z) {
-	this.x = (typeof x == "undefined") ? 0 : x;
-	this.y = (typeof y == "undefined") ? 0 : y;
-	this.z = (typeof z == "undefined") ? 0 : z;
-}
